09a7d9935704f2553140bfb54cab8d737a6bcd0d
Sonal Ranjit <sonal.ranjit3@gmail.com>
2016-12-18 12:15:26 -0500

    added timeout in between wget calls

--- createWarc.py
+++ createWarc.py
@@ -1,6 +1,7 @@
 import subprocess
 import csv
 import os
+import time
 
 #Input = subprocess.call(["wget", "--warc-file", "/Users/sonal/SideProjects/WarcCreator/my-warc-file.warc", "https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/details?eisId=219321"])
 
@@ -30,6 +31,9 @@ with open(csv_Path) as csvfile:
             os.makedirs(LettersOutputPath)
             #wget has a option flag to pacakge the response in WARC format, unfortunately when supplying multiple urls to one singl WARC, the documents are corrupted
             #therefore they are grabbed in 3 different wget calls
-            html = subprocess.call(["wget", "--warc-file", warcHTMLOutputPath + row['eis_id'], epaurl + row['eis_id'], "--delete-after"])
-            letterDocs = subprocess.call(["wget", base_Letters_EPA_url + row['eis_id'], "-O" + LettersOutputPath + row['eis_id'] + ".zip"])
-            eisDocs = subprocess.call(["wget", base_Documents_EPA_url + row['eis_id'],"-O"+DocumentsOutputPath + row['eis_id'] + ".zip"])
+            subprocess.call(["wget", "--warc-file", warcHTMLOutputPath + row['eis_id'], epaurl + row['eis_id'], "--delete-after"])
+            time.sleep(5)
+            subprocess.call(["wget", base_Letters_EPA_url + row['eis_id'], "-O" + LettersOutputPath + row['eis_id'] + ".zip"])
+            time.sleep(5)
+            subprocess.call(["wget", base_Documents_EPA_url + row['eis_id'],"-O"+DocumentsOutputPath + row['eis_id'] + ".zip"])
+            time.sleep(5)
