8aeb82c4c97cc5ffae01a06182162f7ecaec4733
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-02-05 17:05:21 -0500

    Cleaned up code

--- csv2Urls.py
+++ csv2Urls.py
@@ -1,13 +1,16 @@
 import csv
 
+#path to csv
 csv_Path = 'eis-listing.csv'
+
+#Base url
 base_HTML_EPA_url = 'https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/details?eisId='
 
 with open(csv_Path) as csvfile:
     reader = csv.DictReader(csvfile)
-    text_file = open("EISIds.txt", "w")
+    text_file = open("EIS_URLs.txt", "w")
     for row in reader:
-        epaurl = 'eisId='+row['eis_id']
+        epaurl =  base_HTML_EPA_url+row['eis_id']
         text_file.write(epaurl + '\n')
 
 text_file.close()
